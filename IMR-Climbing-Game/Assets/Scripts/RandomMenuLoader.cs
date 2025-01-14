727a100b334a86f2309eeee84bdd4a774539f929
Grozea Tatiana <taneagrozea2003@gmail.com>
2025-01-14 21:44:24 +0200

    added xr to menus

--- IMR-Climbing-Game/Assets/Scripts/RandomMenuLoader.cs
+++ IMR-Climbing-Game/Assets/Scripts/RandomMenuLoader.cs
@@ -6,9 +6,9 @@ public class RandomMenuLoader : MonoBehaviour
     void Start()
     {
         // Randomly load one of the menu scenes
-        string[] menuScenes = { 
-            "MountainMenu", 
-            //"CityMenu" 
+        string[] menuScenes = {
+            "MountainMenu",
+            "CityMenu"
         };
         string selectedScene = menuScenes[Random.Range(0, menuScenes.Length)];
         SceneManager.LoadScene(selectedScene);
