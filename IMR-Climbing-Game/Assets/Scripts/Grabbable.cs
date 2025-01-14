cbd4bffc643ece1ae9e7344cc74f4024869750e8
MediocreKarma <matei.da.best@gmail.com>
2025-01-14 19:24:19 +0200

    add last lab updates

--- IMR-Climbing-Game/Assets/Scripts/Grabbable.cs
+++ IMR-Climbing-Game/Assets/Scripts/Grabbable.cs
@@ -13,19 +13,24 @@ public class GrabbableObject : MonoBehaviour
     [Tooltip("The object associated with the right controller of the XR Rig")]
     public HandGrabController rightController;
 
-    [Tooltip("The Jump")]
+    [Tooltip("The Jumper used by the player")]
     public HandDirectedJump jumper;
+
+    [Tooltip("The CharacterController of the player.")]
+    public CharacterController characterController;
 
     private ClimbInteractable climbInteractable = null;
     private readonly bool[] previouslyGrabbed  = { false, false };
     private readonly bool[] activelyGrabbing   = { false, false };
     private readonly HandGrabController[] controllers = { null, null };
+    private float radius;
 
     void Start()
     {
         climbInteractable = GetComponent<ClimbInteractable>();
         controllers[0] = leftController;
         controllers[1] = rightController;
+        radius = characterController.radius;
     }
 
     private void HandleGrabbingStateChange(int index)
@@ -66,6 +71,14 @@ public class GrabbableObject : MonoBehaviour
     {
         activelyGrabbing[0] = XRSelectInteractableExtensions.IsSelectedByLeft(climbInteractable);
         activelyGrabbing[1] = XRSelectInteractableExtensions.IsSelectedByRight(climbInteractable);
+        if (activelyGrabbing[0] || activelyGrabbing[1]) 
+        {
+            characterController.radius = 0.01F;
+        }
+        else
+        {
+            characterController.radius = this.radius;
+        }
         HandleGrabbingStateChange(0);
         HandleGrabbingStateChange(1);
 
--- IMR-Climbing-Game/Assets/Scripts/RandomMenuLoader.cs
+++ IMR-Climbing-Game/Assets/Scripts/RandomMenuLoader.cs
@@ -6,7 +6,10 @@ public class RandomMenuLoader : MonoBehaviour
     void Start()
     {
         // Randomly load one of the menu scenes
-        string[] menuScenes = { "MountainMenu", "CityMenu" };
+        string[] menuScenes = { 
+            "MountainMenu", 
+            //"CityMenu" 
+        };
         string selectedScene = menuScenes[Random.Range(0, menuScenes.Length)];
         SceneManager.LoadScene(selectedScene);
     }
